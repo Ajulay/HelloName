29637fdf1fbdbcb1068e6b6258e9a684b8f199c4
Ajulay <11v09v@gmail.com>
2018-12-19 14:20:13 +0300

    Initial commit

---
+++ src/main/java/com/ajulay/Main.java
@@ -0,0 +1,12 @@
+package com.ajulay;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length > 0){
+            System.out.println("Hello, " + args[0] + "!");
+        }else{
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
