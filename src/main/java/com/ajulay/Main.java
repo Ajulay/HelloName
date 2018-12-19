75e7104716eb4ff273e2875007bf28b0da0ead14
Ajulay <11v09v@gmail.com>
2018-12-19 14:55:46 +0300

    Изменен способ введения имени.

--- src/main/java/com/ajulay/Main.java
+++ src/main/java/com/ajulay/Main.java
@@ -1,15 +1,14 @@
 package com.ajulay;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String s = "";
-            for (int i = 0; i < args.length; i++) {
-                s += args[i] + " ";
-            }
-            s = s.substring(0, s.length()-1);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name...");
+        String s = sc.nextLine().trim();
+        if (s.length() > 0) {
             System.out.println("Hello, " + s + "!");
         } else {
             System.out.println("Hello, World!");
